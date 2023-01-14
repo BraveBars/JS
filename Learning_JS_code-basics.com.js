fa2cdbd56d7f436a8bfe3d6f7e6a98f6abb0cbab
Barsukov Sergey <BraveBars@gmail.com>
2023-01-14 23:43:29 +0300

    feat: Task_24

--- Learning_JS_code-basics.com.js
+++ Learning_JS_code-basics.com.js
@@ -103,3 +103,17 @@ console.log(dollarsCount)
 let rublesCount = dollarsCount * 60;
 console.log(rublesCount)
 
+/*24. Напишите программу, которая будет генерировать заголовок и тело письма, используя уже готовые переменные, и выводить получившиеся строки на экран.
+Для заголовка используйте переменные firstName и greeting, запятую и восклицательный знак. Выведите это на экран в правильном порядке.
+Для тела письма используйте переменные info и intro, при этом второе предложение должно быть на новой строке.
+Результат на экране будет выглядеть так:
+Hello, Joffrey!
+Here is important information about your account security.
+We couldn't verify your mother's maiden name.
+Выполните задание, используя только два console.log().*/
+let info = "We couldn't verify your mother's maiden name.";
+let intro = 'Here is important information about your account security.';
+let firstName = 'Joffrey';
+let greeting = 'Hello';
+console.log(greeting + ', ' + firstName + '!')
+console.log(intro + '\n' + info)
