4ef664d19e49ada07fde97675b42e78905fa55fc
Sergey <92271554+BraveBars@users.noreply.github.com>
2023-01-14 09:10:11 +0300

    feat: Task 2

--- Learning JS code-basics.com.js
+++ Learning JS code-basics.com.js
@@ -1,2 +1,5 @@
 //1.Наберите в редакторе код из задания символ в символ и нажмите «Проверить».
 console.log('Hello, World!');
+
+//2. Создайте однострочный комментарий с текстом: You know nothing, Jon Snow!.
+// You know nothing, Jon Snow!
