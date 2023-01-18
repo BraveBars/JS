90a83a758db56f4eab93d7ed84d633e5b4f8b8be
Barsukov Sergey <BraveBars@gmail.com>
2023-01-18 13:19:38 +0300

    feat: Task_59

--- Learning_JS_code-basics.com.js
+++ Learning_JS_code-basics.com.js
@@ -325,6 +325,23 @@ const guessNumber = (number) => {
   if (number === 42) {
     return "You win!";
   }
+  
   return "Try again!";
 };
 console.log(guessNumber(41));
+
+/*59. Реализуйте функцию normalizeUrl(), которая выполняет так называемую нормализацию данных. Она принимает адрес сайта и возвращает его с https:// в начале.
+Функция принимает адреса в виде АДРЕС или https://АДРЕС, но всегда возвращает адрес в виде https://АДРЕС
+Можно использовать метод startsWith(), чтобы проверить, начинается ли строка с префикса https://. А потом на основе этого добавлять или не добавлять https://. */ 
+const normalizeUrl = (url) => {
+  let noramalizedUrl;
+
+  if (url.startsWith("https://")) {
+    noramalizedUrl = url;
+  } else {
+    noramalizedUrl = "https://" + url;
+  }
+
+  return noramalizedUrl;
+};
+console.log(normalizeUrl("fdfdfwefwef"));
