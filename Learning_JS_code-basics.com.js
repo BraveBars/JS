146efe5856472624dd83685f74ceae647d4626e2
Barsukov Sergey <BraveBars@gmail.com>
2023-01-19 08:47:13 +0300

    feat: Task_62

--- Learning_JS_code-basics.com.js
+++ Learning_JS_code-basics.com.js
@@ -393,3 +393,26 @@ const convertText = (text) => {
 console.log(convertText('Fgvbhjn'));
 console.log(convertText(''));
 console.log(convertText('fgvbhjn'));
+
+/*62. Реализуйте функцию getNumberExplanation(), которая принимает на вход число и возвращает объяснение этого числа. Если для числа нет объяснения, то возвращается null:
+getNumberExplanation(8);   // null
+// Объяснения есть только для указанных ниже чисел
+getNumberExplanation(666); // 'devil number'
+getNumberExplanation(42);  // 'answer for everything'
+getNumberExplanation(7);   // 'prime number' */
+const getNumberExplanation = (number) => {
+  switch (number) {
+    case 666:
+      return "devil number";
+    case 42:
+      return "answer for everything";
+    case 7:
+      return "prime number";
+    default:
+      return null;
+  }
+};
+console.log(getNumberExplanation(666));
+console.log(getNumberExplanation(42));
+console.log(getNumberExplanation(7));
+console.log(getNumberExplanation(5));
