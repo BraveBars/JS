3604e0584d882a21e809a8cb65ffd002dafe6703
Barsukov Sergey <BraveBars@gmail.com>
2023-01-24 18:27:54 +0300

    feat: Task_67

--- Learning_JS_code-basics.com.js
+++ Learning_JS_code-basics.com.js
@@ -473,3 +473,19 @@ const printReversedWordBySymbol = (word) => {
 const word = 'Arya';
 printReversedWordBySymbol(word);
 
+/* 67. Функция из теории учитывает регистр букв. То есть A и a с её точки зрения разные символы. Реализуйте вариант этой же функции, так чтобы регистр букв был не важен:
+countChars('HexlEt', 'e'); // 2
+countChars('HexlEt', 'E'); // 2     */
+const countChars = (str, char) => {
+  let i = 0;
+  let count = 0;
+  while (i < str.length) {
+    if (str[i] === char || str[i].toUpperCase() === char.toUpperCase()) {
+      count = count + 1;
+    }
+    i = i + 1;
+  }
+  return count;
+};
+console.log(countChars('HexlEt', 'e'))
+console.log(countChars('HexlEt', 'E'))
